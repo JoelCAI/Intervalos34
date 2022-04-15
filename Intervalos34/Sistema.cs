4de140b6def956f8753ef3c7633ed7bc7fc7de4e
JoelCAI <joelargentina12345@gmail.com>
2022-04-15 11:36:14 -0300

    Agregar archivos de proyecto.

---
+++ Intervalos34/Sistema.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervalos34
+{
+    public class Sistema
+    {
+       
+        public void MenuPrincipal()
+        {
+            Validador.Bienvenida();
+            
+            TimerCallback llamada = new TimerCallback(Tick);
+
+            Console.WriteLine("\n Fecha Actual: {0}\n",
+                               /* DateTime.Now.ToString("hh:mm:ss"));*/
+                               DateTime.Now.ToLongDateString());
+            
+            Timer pulso = new Timer(llamada, null, 0, 10000);
+
+            Thread.Sleep(59000);
+
+        }
+
+        static public void Tick(Object estadoInformativo)
+        {
+            Console.WriteLine("\n Tick: {0}", DateTime.Now.ToLongDateString());
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
---
+++ Intervalos34/Validador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intervalos34
+{
+    public class Validador
+    {
+       
+        public static void Bienvenida()
+        {
+            Console.WriteLine("\n Bienvenido al Programa");
+        }
+    }
+}
